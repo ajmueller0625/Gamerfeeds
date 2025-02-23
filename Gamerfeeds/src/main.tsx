30e91a62affe0ea48eb1a29c8f1a702124f0ceb7
Alfred Mueller <aj.mueller@hotmail.com>
2025-02-23 16:33:16 +0100

    Created new vite project

---
+++ Gamerfeeds/src/main.tsx
@@ -0,0 +1,19 @@
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { StrictMode } from "react";
+import "./index.css";
+import Layout from "./pages/Layout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: []
+  }
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
